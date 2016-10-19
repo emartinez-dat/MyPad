7ac7043b0a5f4dd2121b7570b44c9dd81bffe2bd
E. Martinez <mail@devmartinez.net>
2016-10-19 19:08:01 -0400

    -App updates

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
-function savePad(pad){
-  document.getElementById('mypad').value;
-  function urlPad(data){
-   return "data: x-application/text," + escape(data);
-  }
-  window.open(urlPad(pad));
-
-}
+// function savePad(pad){
+//   var data = document.getElementById('mypad').value;
+//   function urlPad(data){
+//    return "type: text/plain," + escape(data);
+//   }
+//   window.open(urlPad(pad));
+//
+// }
 
 function clearPad(){
   document.getElementById('mypad').value="";
+}
+
+function savePad(pad){
+  var data = document.getElementById('mypad').value;
+  if (data == ""){
+    window.alert('Pad is blank. Try to add some content');
+  }
+  else{
+      var txtData = new Blob([data], {type:'text/plain'});
+  }  
+
 }
