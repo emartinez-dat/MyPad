5dcf94d97ecf1f6ce9cc8f0eaf155dac8608a653
devmartinez <mail@devmartinez.net>
2017-01-17 11:56:44 -0400

    MyPad - App updates

--- js/app.js
+++ js/app.js
@@ -8,16 +8,35 @@
 // }
 
 function clearPad(){
-  document.getElementById('mypad').value="";
-}
-
-function savePad(pad){
-  var data = document.getElementById('mypad').value;
-  if (data == ""){
-    window.alert('Pad is blank. Try to add some content');
+  var pad = $('#mypad').val();
+  if (!pad){
+    swal("Pad Empty", "Pad doesn't have content", "warning");
   }
   else{
-      var txtData = new Blob([data], {type:'text/plain'});
-  }  
+    swal({
+          title: "Are you sure?",
+          text: "This process cannot be undone.",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#DD6B55",
+          confirmButtonText: "Clear Pad",
+          closeOnConfirm: false
+        },
+        function(){
+          swal("Pad Cleared", "Pad is now empty", "success");
+          $('#mypad').val('');
+        });
+  }
+} // END function clearPad
+
+function savePad(){
+  var pad = $('#mypad').val();
+  if (!pad){
+    swal("Pad Empty", "Try to add some content before saving it.", "warning");
+  }
+  else{
+    $('#download').removeAttribute('disabled');
+    var txtData = new Blob([data], {type:'text/plain'});
+  }
 
 }
