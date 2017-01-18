eed678f81ec0e123838973fdfb05baf1cf20f769
devmartinez <mail@devmartinez.net>
2017-01-18 01:18:46 -0400

    MyPad - App updates

--- js/app.js
+++ js/app.js
@@ -1,42 +1,71 @@
-// function savePad(pad){
-//   var data = document.getElementById('mypad').value;
-//   function urlPad(data){
-//    return "type: text/plain," + escape(data);
-//   }
-//   window.open(urlPad(pad));
-//
-// }
+/*
+App Name: MyPad
+Version: 1.0
+Author: Edwin Martinez
+Website: http://www.devmartinez.net
+Comments: Web app for write down some text, simple and light.
+ */
 
-function clearPad(){
+//This function set the focus on the pad area
+function setFocus(){
+  $('#mypad').focus();
+}// END function setFocus
+
+/*
+This function checks the pad for input. If it is some input
+the save button is enabled. Otherwise, the button is disabled.
+ */
+function checkPad(){
   var pad = $('#mypad').val();
-  if (!pad){
-    swal("Pad Empty", "Pad doesn't have content", "warning");
+  if(!pad){
+    $('#download').prop("disabled", true);
+    $('#clear').prop("disabled", true);
   }
   else{
-    swal({
-          title: "Are you sure?",
-          text: "This process cannot be undone.",
-          type: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#DD6B55",
-          confirmButtonText: "Clear Pad",
-          closeOnConfirm: false
-        },
-        function(){
-          swal("Pad Cleared", "Pad is now empty", "success");
-          $('#mypad').val('');
-        });
+    $('#download').prop("disabled", false);
+    $('#clear').prop("disabled", false);
   }
+}// END function checkPad
+
+//This function cleans the pad. The information cannot be retrieved.
+function clearPad(){
+  swal({
+        title: "Are you sure?",
+        text: "This process cannot be undone.",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Clear Pad",
+        closeOnConfirm: false
+      },
+      function(){
+        setFocus();
+        $('#download').prop("disabled", true);
+        $('#clear').prop("disabled", true);
+        swal("Pad Cleared", "Pad is now empty", "success");
+        $('#mypad').val('');
+      });
 } // END function clearPad
 
+//This function let's you save the content of the pad as text file
 function savePad(){
   var pad = $('#mypad').val();
-  if (!pad){
-    swal("Pad Empty", "Try to add some content before saving it.", "warning");
-  }
-  else{
-    $('#download').removeAttribute('disabled');
-    var txtData = new Blob([data], {type:'text/plain'});
-  }
+  var date = new Date();
+  var stringDate = moment(date).format("MMMM DD YYYY h:mm a");
+  var filename = "pad-" + stringDate +".txt";
+  var blob = new Blob([pad], {type: "text/plain;charset=utf-8"});
+  swal({
+        title: "File is ready",
+        text: "Click Ok to download the file.",
+        type: "success",
+        showCancelButton: true,
+        closeOnConfirm: true
+      },
+      function(){
+        saveAs(blob, filename);
+      });
+} //END function savePad
 
-}
+
+
+
